7b94c24d0debe3505befb53c92109e735c3d585f
Abdul Moeez Asad <aasad.bscs22seecs@seecs.edu.pk>
2025-02-18 05:06:59 +0500

    feat: button for eis addeed (content to be added)

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -15,6 +15,8 @@ import BatteryInfoCard from "@/components/BatteryInfoCard";
 import * as Animatable from "react-native-animatable";
 import { fetchAPI, useFetch } from "@/lib/fetch";
 import { RotateCcw } from "lucide-react-native";
+import * as Progress from "react-native-progress";
+
 Animatable.initializeRegistryWithDefinitions({
   translateY: {
     from: { translateY: 0 },
@@ -28,17 +30,17 @@ Animatable.initializeRegistryWithDefinitions({
 
 const Home = () => {
   const [showInfo, setShowInfo] = useState<batteryProps>();
+  const [getEis, setGetEis] = useState(false);
+  const [getEisProgress, setGetEisProgress] = useState(0);
   const [refresh, setRefresh] = useState(false);
   const { data, loading, refetch } = useFetch(
     "https://united-whippet-openly.ngrok-free.app/get_dict/0"
   );
   const carData: batteryListI = (data || {}) as batteryListI;
-  if (loading) {
-    return (
-      <View className="flex h-full justify-center items-center">
-        <ActivityIndicator size="small" color="#000" />
-      </View>
-    );
+  if (getEis) {
+    setTimeout(() => {
+      setGetEisProgress((prev) => prev + 0.1);
+    }, 1000);
   }
   if (!refresh && !loading) {
     setTimeout(() => {
@@ -52,7 +54,7 @@ const Home = () => {
         <RefreshControl refreshing={loading} onRefresh={refetch} />
       }
     >
-      {refresh && (
+      {refresh && !loading && (
         <Animatable.View animation="translateBack">
           <TouchableOpacity
             onPress={() => {
@@ -65,59 +67,97 @@ const Home = () => {
           </TouchableOpacity>
         </Animatable.View>
       )}
-      <View className="flex flex-1 mt-16 justify-center items-center">
-        <Image source={images.car} resizeMode="contain" />
-        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-          <View className="flex flex-row justify-center items-center space-x-1 px-2 pt-24">
-            {Object?.entries(carData)
-              ?.filter(([key]) => key.startsWith("b"))
-              ?.map(([key, charge], index) => (
-                <Animatable.View
-                  key={index}
-                  className="items-center"
-                  animation={
-                    showInfo?.id === key ? "translateY" : "translateBack"
-                  }
-                >
-                  <TouchableOpacity
-                    onPress={() => {
-                      setShowInfo({
-                        id: key,
-                        health: charge,
-                        name: "",
-                        cdl: 0,
-                        rct: 0,
-                        re: 0,
-                        warns: [],
-                      });
-                    }}
-                  >
-                    <CustomBatterySVG
-                      charge={charge as number}
-                      width={60}
-                      height={100}
-                    />
-                  </TouchableOpacity>
-                  <Text className="text-center font-JakartaBold">
-                    {charge}%
-                  </Text>
-                </Animatable.View>
-              ))}
+      <View className="flex flex-1 mt-16 justify-center items-center h-full">
+        <Image
+          source={images.car}
+          resizeMode="contain"
+          className={`${loading ? "mt-32" : ""}`}
+        />
+        {loading ? (
+          <View className="flex h-full justify-center items-center">
+            <ActivityIndicator size="small" color="#000" />
           </View>
-        </ScrollView>
-        <View className="flex flex-row flex-1 justify-center items-center mt-14 px-4">
-          <BatteryInfoCard
-            img={images.fan}
-            heading="Temperature"
-            cardStyles="mr-8 flex-1"
-            description={`${carData.temp}°C`}
-          />
-          <BatteryInfoCard
-            img={images.range}
-            heading="Range"
-            cardStyles="flex-1"
-            description={`${carData.range} mi`}
-          />
+        ) : (
+          <>
+            <ScrollView
+              horizontal={true}
+              showsHorizontalScrollIndicator={false}
+            >
+              <View className="flex flex-row justify-center items-center space-x-1 px-2 pt-24">
+                {Object?.entries(carData)
+                  ?.filter(([key]) => key.startsWith("b"))
+                  ?.map(([key, charge], index) => (
+                    <Animatable.View
+                      key={index}
+                      className="items-center"
+                      animation={
+                        showInfo?.id === key ? "translateY" : "translateBack"
+                      }
+                    >
+                      <TouchableOpacity
+                        onPress={() => {
+                          setShowInfo({
+                            id: key,
+                            health: charge,
+                            name: "",
+                            cdl: 0,
+                            rct: 0,
+                            re: 0,
+                            warns: [],
+                          });
+                        }}
+                      >
+                        <CustomBatterySVG
+                          charge={charge as number}
+                          width={60}
+                          height={100}
+                        />
+                      </TouchableOpacity>
+                      <Text className="text-center font-JakartaBold">
+                        {charge}%
+                      </Text>
+                    </Animatable.View>
+                  ))}
+              </View>
+            </ScrollView>
+            <View className="flex flex-row flex-1 justify-center items-center mt-14 px-4">
+              <BatteryInfoCard
+                img={images.fan}
+                heading="Temperature"
+                cardStyles="mr-8 flex-1"
+                description={`${carData.temp}°C`}
+              />
+              <BatteryInfoCard
+                img={images.range}
+                heading="Range"
+                cardStyles="flex-1"
+                description={`${carData.range} mi`}
+              />
+            </View>
+          </>
+        )}
+        <View className="w-full px-4 mt-6 relative">
+          <TouchableOpacity
+            onPress={() => {
+              setGetEis(true);
+            }}
+            className={` bg-secondary-700 rounded-xl p-4 ${getEis ? " opacity-0" : "opacity-100"} transition-all duration-100 ease-in-out`}
+          >
+            <Text className="text-center font-JakartaBold text-white text-md">
+              Get Eis Progress
+            </Text>
+          </TouchableOpacity>
+          <View
+            className={`absolute top-4 left-4 ${getEis ? "opacity-100" : "opacity-0"}`}
+          >
+            <Progress.Bar
+              progress={getEisProgress}
+              width={500}
+              animated={true}
+              className="mx-auto max-w-full"
+              color="#666666"
+            />
+          </View>
         </View>
       </View>
     </ScrollView>
